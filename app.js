781ec9baeffbd275e520d777ce584efa0acd2df9
NOYEAH <labslave069@gmail.com>
2024-11-15 19:05:11 +0900

    2차테스트

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 const path = require('path');
 
 // 포트 번호 설정 (예: 3000번 포트)
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // HTML 파일 제공 경로 설정
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,3 +19,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
